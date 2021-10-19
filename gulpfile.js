c7a47431b58657e8a29d905a3160412622d06889
iarekeng <kenglee87@gmail.com>
2021-10-19 09:21:33 -0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,8 @@ function watch() {
       baseDir: './'
     }
   });
+
+
   gulp.watch('./scss/**/*.scss', style);
   gulp.watch('./css/**/*.css').on('change', browserSync.reload)
   gulp.watch('./*.html').on('change', browserSync.reload);
