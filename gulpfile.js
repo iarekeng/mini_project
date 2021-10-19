7982ba985ec09568051d3100d0a5b7ed0ed90f27
iarekeng <kenglee87@gmail.com>
2021-10-19 10:36:42 -0700

    test5

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,26 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+   uglify = require('gulp-uglify'),
+   sass = require('gulp-sass'),
+   browserSync = require('browser-sync').create();
 
+gulp.task('js', function () {
+    return gulp.src('js/*.js')
+       .pipe(jshint())
+       .pipe(jshint.reporter('default'))
+       .pipe(uglify())
+       .pipe(concat('app.js'))
+       .pipe(gulp.dest('build'));
+ });
 
+ 
+ gulp.task('default', function(cb) {
+cb();
+ })
 
-function style() {
-  return gulp.src('./scss/**/*.scss')
+gulp.task('sass', function() {
+  gulp.src('./scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-}
+})
 
-function watch() {
-  browserSync.init({
-    injectChanges: true,
-    server: {
-      baseDir: './'
-    }
-  });
-
-  gulp.watch('./scss/**/*.scss', style);
-  gulp.watch('./css/**/*.css').on('change', browserSync.reload)
-  gulp.watch('./*.html').on('change', browserSync.reload);
-}
-
-function build(cb) {
-  cb();
-}
-
-
-exports.watch = watch;
-exports.default = style;
-exports.build = build;
